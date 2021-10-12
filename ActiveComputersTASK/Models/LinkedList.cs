edc00ee8d7450bbb0d05e4d0fc211ec0a015830d
Farhad Mammadov <farhard_group@mail.ru>
2021-10-12 21:20:03 +0400

    Finished Tasks

---
+++ ActiveComputersTASK/Models/LinkedList.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace ActiveComputersTASK.Models
+{
+    public class LinkedList
+    {
+
+
+        public Node head1, head2;
+
+        public class Node
+        {
+            public int data;
+            public Node next;
+
+            public Node(int d)
+            {
+                data = d;
+                next = null;
+            }
+        }
+
+
+        public Node addTwoLists(Node first, Node second)
+        {
+
+            Node res = null;
+            Node prev = null;
+            Node temp = null;
+            int carry = 0, sum;
+
+
+            while (first != null || second != null)
+            {
+
+                sum = carry + (first != null ? first.data : 0)
+                      + (second != null ? second.data : 0);
+
+
+                carry = (sum >= 10) ? 1 : 0;
+
+
+                sum = sum % 10;
+
+
+                temp = new Node(sum);
+
+
+                if (res == null)
+                {
+                    res = temp;
+                }
+
+
+                else
+                {
+                    prev.next = temp;
+                }
+
+
+                prev = temp;
+
+
+                if (first != null)
+                {
+                    first = first.next;
+                }
+                if (second != null)
+                {
+                    second = second.next;
+                }
+            }
+
+            if (carry > 0)
+            {
+                temp.next = new Node(carry);
+            }
+
+
+            return res;
+        }
+
+
+        public void printList(Node head)
+        {
+            while (head != null)
+            {
+                Console.Write(head.data + " ");
+                head = head.next;
+            }
+            Console.WriteLine("");
+        }
+    }
+}
---
+++ ActiveComputersTASK/Program.cs
@@ -0,0 +1,95 @@
+﻿using ActiveComputersTASK.Models;
+using System;
+using static ActiveComputersTASK.Models.LinkedList;
+
+namespace ActiveComputersTASK
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //1.Реализовать приложение C#, которое предоставит
+            //    функционал сложения каждого второго нечетного 
+            //    числа из массива чисел тела запроса и вернёт 
+            //    их сумму по модулю.
+            //Console.WriteLine(Ex1(new int[] { 2, -3, 4, -7, 10, -11, 112, -15, 16, -19 }));
+
+            //2.2. Реализовать приложение C#, которое предоставит функционал сложения двух непустых связанных списков
+            //, представляющих два положительных целых числа. Цифры хранятся в обратном порядке, каждый из их узлов
+            //содержит одну цифру.Реализуйте алгоритм сложения связанных списков,
+            //    не переводя хранящиеся в них данные в числовой тип(по - узловое сложение), 
+            //    возвращающий результат в виде связанного списка.
+
+            //EX2 
+
+            LinkedList list = new LinkedList();
+
+            // Первый лист
+            list.head1 = new Node(7);
+            list.head1.next = new Node(5);
+            list.head1.next.next = new Node(9);
+            list.head1.next.next.next = new Node(4);
+            list.head1.next.next.next.next = new Node(6);
+            Console.Write("первый");
+            list.printList(list.head1);
+            // Второй лист
+            list.head2 = new Node(8);
+            list.head2.next = new Node(4);
+            Console.Write("Второй");
+            list.printList(list.head2);
+            //Резултать
+            Node rs = list.addTwoLists(list.head1, list.head2);
+            Console.Write("Лист1 + лист 2");
+            list.printList(rs);
+
+
+            //Ex3
+
+            //3. Реализовать приложение C#,
+            //которое предоставит функционал определения
+            //входящий строки является ли она палиндромом.
+
+            //Console.WriteLine(IsPalindrom("yaya"));
+
+            Console.ReadKey(true);
+
+        }
+
+        //Ex1
+        //public static int Ex1(int[] n)
+        //{
+        //    List<int> oddList = new List<int>();
+        //    int sum = 0;
+        //    for (int i = 0; i < n.Length; i++)
+        //    {
+        //        if (n[i] % 2 == 0)
+        //        {
+        //            continue;
+        //        }
+        //        oddList.Add(n[i]);
+        //    }
+        //    sum = oddList.Where((x, i) => i % 2 != 0).Sum();
+        //    return sum;
+        //}
+
+        //Ex2
+
+
+
+
+        //Ex3 
+
+        //private static bool IsPalindrom(string str)
+        //{
+        //    for (int i = 0; i < str.Length; i++)
+        //        if (str[i] != str[str.Length - i - 1]) return false;
+        //    return true;
+        //}
+
+
+
+
+
+    }
+}
